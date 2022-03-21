2d917727ff70f1c3443ab3d2b34305881163a0e7
dmitro <dmitrii_zhurzhalin@mail.ru>
2022-03-21 19:29:27 +0300

    refactoring 21_03_22

--- src/CarListTest.java
+++ src/CarListTest.java
@@ -7,9 +7,9 @@ public class CarListTest {
 
     @org.junit.Before
     public void setUp() throws Exception {
-        //init
+        carList = new CarArrayList();
         for (int i = 0; i < 100; i++) {
-            carList.add(new Car("brand" + i, i));
+            carList.add(new Car("Brand" + i, i));
         }
     }
 
